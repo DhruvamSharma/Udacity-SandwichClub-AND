2a13aeebffde8a9db1200b5254d5c2205f2fe792
Dhruvam <dhruvamssharma@gmail.com>
2018-05-30 04:01:55 +0530

    my-commit

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,30 +3,49 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    TextView mOriginTV, mAlsoKnownAsTV, mIngredientsTV, mDescription, mMainNameTV;
+    TextView mAlsoKnownAsLabel, mIngredientsLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        /**
+         * FindViewById Calls to different views in Detail Activity.
+         */
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        mOriginTV = findViewById(R.id.origin_tv);
+        mAlsoKnownAsTV = findViewById(R.id.also_known_tv);
+        mIngredientsTV = findViewById(R.id.ingredients_tv);
+        mDescription = findViewById(R.id.description_tv);
+        mMainNameTV = findViewById(R.id.main_name_tv);
+
+        mAlsoKnownAsLabel = findViewById(R.id.also_known_label);
+        mIngredientsLabel = findViewById(R.id.ingredients_label);
 
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
         }
 
+        assert intent != null;
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
@@ -37,13 +56,15 @@ public class DetailActivity extends AppCompatActivity {
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
         Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+
         if (sandwich == null) {
+
             // Sandwich data unavailable
             closeOnError();
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +77,38 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    /**
+     * Method to populate data of different views
+     */
+    private void populateUI(Sandwich sandwich) {
+        mMainNameTV.setText(sandwich.getMainName());
+        mOriginTV.setText(sandwich.getPlaceOfOrigin());
+        mDescription.setText(sandwich.getDescription());
 
+        checkAndSetList(sandwich.getIngredients(), mIngredientsTV, mIngredientsLabel);
+        checkAndSetList(sandwich.getAlsoKnownAs(), mAlsoKnownAsTV, mAlsoKnownAsLabel);
+    }
+
+
+    /**
+     * Method to check list size.
+     * If size is not 0, populate the Ingredient TextView
+     * @param list
+     */
+
+    private void checkAndSetList(List<String> list, TextView dynamicTV, TextView dynamicLabel) {
+        if(list.isEmpty()) {
+            dynamicTV.setVisibility(View.GONE);
+            dynamicLabel.setVisibility(View.GONE);
+            return;
+        }
+        StringBuilder data = new StringBuilder();
+        for( int i =0; i <list.size(); i++) {
+            data.append(list.get(i));
+            if ( i != list.size()-1)
+            data.append(",");
+        }
+
+        dynamicTV.setText(data.toString());
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,62 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        /**
+         * Step 1: Creating a sandwich object to fill data with and send back to the calling method.
+         * Step 2: Parsing the json data and setting it in the newly created sandwich object.
+         * Step 3: Sending the sanswich object back.
+         */
+
+        /**
+         * Created a helper method jsonToStringArray to convert jsonArray to String Array.
+         */
+
+        Sandwich sandwich = new Sandwich();
+
+        // parsing json
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+
+            sandwich.setMainName(jsonObject.getJSONObject("name").getString("mainName"));
+            sandwich.setAlsoKnownAs( jsonToStringArray(jsonObject.getJSONObject("name").getJSONArray("alsoKnownAs")) );
+            sandwich.setIngredients( jsonToStringArray(jsonObject.getJSONArray("ingredients")) );
+            sandwich.setDescription( jsonObject.getString("description"));
+            sandwich.setImage( jsonObject.getString("image"));
+            sandwich.setPlaceOfOrigin( jsonObject.getString("placeOfOrigin"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return sandwich;
+    }
+
+
+
+
+
+    // HELPER METHODS
+
+    private static List<String> jsonToStringArray(JSONArray array) {
+        List<String> list = new ArrayList<>();
+
+        for( int i =0; i < array.length() ; i++ ) {
+            try {
+                list.add(array.getString(i));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return list;
     }
 }
